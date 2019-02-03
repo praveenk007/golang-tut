5cb4ed60ba07bc7519964d98bb72b22831c83659
pk1992 <praveen.k@gmail.com>
2019-02-03 11:50:40 +0530

    xml parsing example

---
+++ tut-11 xml-parse/parse.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+type Person struct {
+	Name      string     `xml:"name"`
+	Mobile    string     `xml:"mobile"`
+	Locations []Location `xml:"location"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+func main() {
+	var person Person
+	xmlString := getDummyXml()
+	xml.Unmarshal([]byte(xmlString), &person)
+	fmt.Println(person)
+	fmt.Println("name : ", person.Name, ", mobile : ", person.Mobile, ", location : ", person.Locations)
+}
+
+func getDummyXml() string {
+	return "<person><name>praveen</name><mobile>7977726385</mobile><location><loc>thane</loc></location><location><loc>mulund</loc></location></person>"
+}
